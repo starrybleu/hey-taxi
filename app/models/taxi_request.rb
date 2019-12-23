747a36a9f676c22bebddb634d19290a3c8d0ca25
Hanbyul Lee <starrybleu@gmail.com>
2019-12-23 17:41:45 +0900

    Todo 확인 : 레일즈에서 validate 할 필요 없는 attribute 에 대해서는 model 클래스에 명시할 필요가 없다.

--- app/models/taxi_request.rb
+++ app/models/taxi_request.rb
@@ -2,7 +2,4 @@ class TaxiRequest < ApplicationRecord
   validates_presence_of :passenger_id
   validates_presence_of :address
   validates_presence_of :requested_at
-
-  # todo] driver_id, assigned_at 는 이 클래스에 여전히 남아 있어야 할 것 같은데,
-  # todo] nullable attribute 이기 때문에 validates_presence_of 로 하면 테스트가 실패함. 찾아보기
 end
