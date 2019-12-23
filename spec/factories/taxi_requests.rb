71a9ef32410019d9deade387b74b3570d41d8813
Hanbyul Lee <starrybleu@gmail.com>
2019-12-23 12:16:11 +0900

    TaxiRequest 테스트를 위해 FactoryBot 이용하여 가짜 데이터 형식 설정

---
+++ spec/factories/taxi_requests.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :taxi_request do
+    passenger_id { Faker::Number.number }
+    address { Faker::Lorem.sentence[0..100] }
+    requested_at { Faker::Time..between_dates(from: Date.today - 1, to: Date.today, period: :all)}
+    driver_id nil
+    assigned_at nil
+  end
+end
