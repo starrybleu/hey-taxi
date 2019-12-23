2f363d75223e1fe79f6c4bcc1496b13472a86200
Hanbyul Lee <starrybleu@gmail.com>
2019-12-23 12:33:36 +0900

    TaxiRequest 조회 테스트 작성

---
+++ spec/requests/taxi_requests_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+LIST_SIZE = 10
+
+RSpec.describe 'TaxiRequest API', type: :request do
+  let!(:taxi_requests) { create_list(:taxi_request, LIST_SIZE) }
+
+  describe 'GET /api/taxi-requests' do
+    before { get '/api/tax-requests' }
+
+    it 'returns taxi-requests' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(LIST_SIZE)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+
+  end
+end
