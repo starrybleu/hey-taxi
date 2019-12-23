c3d8d95478b9877d89d6161fe7971cc5305150fa
Hanbyul Lee <starrybleu@gmail.com>
2019-12-23 11:57:41 +0900

    TaxiRequest 모델의 실제 코드 작성하여 pass test

--- app/models/taxi_request.rb
+++ app/models/taxi_request.rb
@@ -1,3 +1,7 @@
 class TaxiRequest < ApplicationRecord
-
+  validates_presence_of :passenger_id
+  validates_presence_of :address
+  validates_presence_of :requested_at
+  validates_presence_of :driver_id
+  validates_presence_of :assigned_at
 end
